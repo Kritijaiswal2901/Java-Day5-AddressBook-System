7992d0d0b0e0c0d7249992669cedc350a6aa72ce
Kriti Jaiswal <kritijaiswal2901@yahoo.com>
2023-11-24 01:59:20 +0530

    Edit contatct by name

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,24 +1,50 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class AddressBookSystem {
 
-    private ArrayList<Contact> contactList;
+    private HashMap<String, Contact> contactList;
 
     public AddressBookSystem() {
-        contactList = new ArrayList<Contact>();
+        contactList = new HashMap<String, Contact>();
     }
-
+    
     public void addContact(Contact contact) {
-        contactList.add(contact);
+        contactList.put(contact.getName(), contact);
+    }
+    
+    public Contact getContactbyName(String name) {
+        if(contactList.containsKey(name)) {
+            return contactList.get(name);
+        }
+        return null;
+    }
+    
+    public void askForEditContact(Scanner sc) {
+        System.out.println("Enter Name to edit Contact: ");
+        String name;
+        name = sc.nextLine();
+        Contact searchContact = this.getContactbyName(name);
+        if(searchContact != null) {
+            searchContact.editContact(sc);
+            System.out.println("Updated Contact: ");
+            searchContact.printContact();
+        } else {
+            System.out.println("No Contact found!");
+        }
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Address Book Program");
 
         AddressBookSystem addressBook = new AddressBookSystem();
         Contact contact1 = new Contact();
-        contact1.inputContact();
-
+        contact1.inputContact(sc);
+        contact1.printContact();
         addressBook.addContact(contact1);
+
+        addressBook.askForEditContact(sc);
+        sc.close();
     }
 }
