eb16c554e9a5f8e86e566da8338f1b41af0bb2bd
Kriti Jaiswal <kritijaiswal2901@yahoo.com>
2023-12-13 22:47:26 +0530

    SQL and ER Diagram

--- queries.sql
+++ queries.sql
@@ -2,23 +2,21 @@
 -- creating new database for address books
 create database address_book_service;
 show databases;
-+---------------------+
-| Database            |
-+---------------------+
-| address_book_system |
-| information_schema  |
-| mysql               |
-| performance_schema  |
-| sys                 |
-+---------------------+
-5 rows in set (0.00 sec)
+
+-- output to the above query
+-- address_book_service
+-- information_schema
+-- mysql
+-- payroll_service
+-- performance_schema
+-- sys
 
 -- UC2
 -- using the created database
-USE address_book_service;
+use address_book_service;
 
 -- creating an address book table
-CREATE TABLE addressBook1 (
+create table address_book_1 (
     first_name varchar(50) not null,
     last_name varchar(50),
     address varchar(100),
@@ -31,152 +29,196 @@ CREATE TABLE addressBook1 (
 
 -- UC3
 -- creating new contacts in address book
-INSERT INTO addressBook1 (first_name, last_name, address, city, state, zip, phone, email) VALUES
+insert into address_book_1 (first_name, last_name, address, city, state, zip, phone, email) values
 	("Kriti", "Jaiswal", "Hazipur", "Patna", "Bihar", "74185", "919936574857", "kritijaiswal7485@gmail.com"),
-    ("Sandhya", "Sharma", "Campel Road", "Amritsar", "Punjab", "456002", "91 9685741452", "sandhyasharma47@gmail.com");
+    ("Sandhya", "Sharma", "Campel Road", "Amritsar", "Punjab", "456002", "91 9685741452", "sandhyasharma47@gmail.com"),
+    ("Archana", "Singh", "pqr", "Ahmedabad", "Gujarat", "380001", "91 945612345", "kush@gmail.com");
+select * from address_book_1;
 
-
-SELECT * FROM addressBook1;
-+------------+-----------+-------------+----------+--------+--------+---------------+----------------------------+
-| first_name | last_name | address     | city     | state  | zip    | phone         | email                      |
-+------------+-----------+-------------+----------+--------+--------+---------------+----------------------------+
-| Kriti      | Jaiswal   | Hazipur     | Patna    | Bihar  | 74185  | 919936574857  | kritijaiswal7485@gmail.com |
-| Sandhya    | Sharma    | Campel Road | Amritsar | Punjab | 456002 | 91 9685741452 | sandhyasharma47@gmail.com  |
-+------------+-----------+-------------+----------+--------+--------+---------------+----------------------------+
-2 rows in set (0.00 sec)
-
+-- output to the above query
+-- Kriti	Jaiswal	Hazipur	Patna	Bihar	74185	919936574857	kritijaiswal7485@gmail.com
+-- Sandhya	Sharma	Campel Road	Amritsar	Punjab	456002	91 9685741452	sandhyasharma47@gmail.com
+-- Archana	Singh	pqr	Ahmedabad	Gujarat	380001	91 945612345	kush@gmail.com
 
 -- UC4
 -- editing the contact using first name
-UPDATE addressBook1 SET address = "Kochi" where first_name = "Kriti";
+update address_book_1 set address = "Samastipur" where first_name = "Kriti";
+select * from address_book_1;
 
-
-SELECT * FROM addressBook1;
-+------------+-----------+-------------+----------+--------+--------+---------------+----------------------------+
-| first_name | last_name | address     | city     | state  | zip    | phone         | email                      |
-+------------+-----------+-------------+----------+--------+--------+---------------+----------------------------+
-| Kriti      | Jaiswal   | Kochi       | Patna    | Bihar  | 74185  | 919936574857  | kritijaiswal7485@gmail.com |
-| Sandhya    | Sharma    | Campel Road | Amritsar | Punjab | 456002 | 91 9685741452 | sandhyasharma47@gmail.com  |
-+------------+-----------+-------------+----------+--------+--------+---------------+----------------------------+
-2 rows in set (0.00 sec)
+-- output to the above query
+-- Kriti	Jaiswal	Samastipur	Patna	Bihar	74185	919936574857	kritijaiswal7485@gmail.com
+-- Sandhya	Sharma	Campel Road	Amritsar	Punjab	456002	91 9685741452	sandhyasharma47@gmail.com
+-- Archana	Singh	pqr	Ahmedabad	Gujarat	380001	91 945612345	kush@gmail.com
 
 -- UC5
 -- deleting a contact using first_name
-DELETE FROM addressBook1 WHERE first_name = "Sandhya";
+delete from address_book_1 where first_name = "Archana";
+select * from address_book_1;
 
-SELECT * FROM addressBook1;
-+------------+-----------+---------+-------+-------+-------+--------------+----------------------------+
-| first_name | last_name | address | city  | state | zip   | phone        | email                      |
-+------------+-----------+---------+-------+-------+-------+--------------+----------------------------+
-| Kriti      | Jaiswal   | Kochi   | Patna | Bihar | 74185 | 919936574857 | kritijaiswal7485@gmail.com |
-+------------+-----------+---------+-------+-------+-------+--------------+----------------------------+
-1 row in set (0.00 sec)
+-- output to the above query
+-- Kriti	Jaiswal	Samastipur	Patna	Bihar	74185	919936574857	kritijaiswal7485@gmail.com
+-- Sandhya	Sharma	Campel Road	Amritsar	Punjab	456002	91 9685741452	sandhyasharma47@gmail.com
 
 -- adding more data to table
-INSERT INTO addressBook1 (first_name, last_name, address, city, state, zip, phone, email) VALUES
+insert into address_book_1 (first_name, last_name, address, city, state, zip, phone, email) values
 	("Anju", "Sharma", "Danapur", "Patna", "Bihar", "784575", "91 9876542581", "anju0174@gmail.com"),
     ("Ishika", "Das", "Adampur", "Patna", "Bihar", "784572", "91 9871473698", "ishika7875@gmail.com");
 
 -- UC6
 -- retrieving people belonging to city
-SELECT * FROM addressBook1 WHERE city = "Patna";
-+------------+-----------+---------+-------+-------+--------+---------------+----------------------------+
-| first_name | last_name | address | city  | state | zip    | phone         | email                      |
-+------------+-----------+---------+-------+-------+--------+---------------+----------------------------+
-| Kriti      | Jaiswal   | Kochi   | Patna | Bihar | 74185  | 919936574857  | kritijaiswal7485@gmail.com |
-| Anju       | Sharma    | Danapur | Patna | Bihar | 784575 | 91 9876542581 | anju0174@gmail.com         |
-| Ishika     | Das       | Adampur | Patna | Bihar | 784572 | 91 9871473698 | ishika7875@gmail.com       |
-+------------+-----------+---------+-------+-------+--------+---------------+----------------------------+
-3 rows in set (0.00 sec)
+select * from address_book_1 where city = "Patna";
+
+-- output to the above query
+-- Kriti	Jaiswal	Samastipur	Patna	Bihar	74185	919936574857	kritijaiswal7485@gmail.com
+-- Anju	Sharma	Danapur	Patna	Bihar	784575	91 9876542581	anju0174@gmail.com
+-- Ishika	Das	Adampur	Patna	Bihar	784572	91 9871473698	ishika7875@gmail.com
 
 -- retrieving people belonging to state
-SELECT * FROM addressBook1 WHERE state = "Bihar";
-+------------+-----------+---------+-------+-------+--------+---------------+----------------------------+
-| first_name | last_name | address | city  | state | zip    | phone         | email                      |
-+------------+-----------+---------+-------+-------+--------+---------------+----------------------------+
-| Kriti      | Jaiswal   | Kochi   | Patna | Bihar | 74185  | 919936574857  | kritijaiswal7485@gmail.com |
-| Anju       | Sharma    | Danapur | Patna | Bihar | 784575 | 91 9876542581 | anju0174@gmail.com         |
-| Ishika     | Das       | Adampur | Patna | Bihar | 784572 | 91 9871473698 | ishika7875@gmail.com       |
-+------------+-----------+---------+-------+-------+--------+---------------+----------------------------+
-3 rows in set (0.00 sec)
+select * from address_book_1 where state = "Bihar";
+
+-- output to the above query
+-- Kriti	Jaiswal	Samastipur	Patna	Bihar	74185	919936574857	kritijaiswal7485@gmail.com
+-- Anju	Sharma	Danapur	Patna	Bihar	784575	91 9876542581	anju0174@gmail.com
+-- Ishika	Das	Adampur	Patna	Bihar	784572	91 9871473698	ishika7875@gmail.com
 
 -- UC7
 -- getting number of contacts by each city
-SELECT city, count(city) FROM addressBook1 GROUP BY city;
-+-------+-------------+
-| city  | count(city) |
-+-------+-------------+
-| Patna |           3 |
-+-------+-------------+
-1 row in set (0.01 sec)
+select city, count(city) from address_book_1 group by city;
+
+-- output to above query
+-- Patna	3
+-- Amritsar	1
 
 -- getting number of contacts by every state
-SELECT state, count(state) FROM addressBook1 GROUP BY state;
-+-------+--------------+
-| state | count(state) |
-+-------+--------------+
-| Bihar |            3 |
-+-------+--------------+
-1 row in set (0.00 sec)
+select state, count(state) from address_book_1 group by state;
+
+-- output to above query
+-- Bihar	3
+-- Punjab	1
 
 -- UC8
 -- retrieve entries sorted by name for particular city
-SELECT * FROM addressBook1 WHERE city = "Patna" ORDER BY first_name ASC;
-+------------+-----------+---------+-------+-------+--------+---------------+----------------------------+
-| first_name | last_name | address | city  | state | zip    | phone         | email                      |
-+------------+-----------+---------+-------+-------+--------+---------------+----------------------------+
-| Anju       | Sharma    | Danapur | Patna | Bihar | 784575 | 91 9876542581 | anju0174@gmail.com         |
-| Ishika     | Das       | Adampur | Patna | Bihar | 784572 | 91 9871473698 | ishika7875@gmail.com       |
-| Kriti      | Jaiswal   | Kochi   | Patna | Bihar | 74185  | 919936574857  | kritijaiswal7485@gmail.com |
-+------------+-----------+---------+-------+-------+--------+---------------+----------------------------+
-3 rows in set (0.00 sec)
+select * from address_book_1 where city = "Patna" order by first_name asc;
+
+-- output to above query
+-- Anju	Sharma	Danapur	Patna	Bihar	784575	91 9876542581	anju0174@gmail.com
+-- Ishika	Das	Adampur	Patna	Bihar	784572	91 9871473698	ishika7875@gmail.com
+-- Kriti	Jaiswal	Samastipur	Patna	Bihar	74185	919936574857	kritijaiswal7485@gmail.com
 
 -- UC 9
 -- adding name and type to address book
-ALTER TABLE addressBook1 ADD column type varchar(20), ADD column name varchar(50);
+alter table address_book_1 add column type varchar(20), add column name varchar(50);
 
 -- adding name and type to all contacts
-UPDATE addressBook1 SET name = "book1";
-UPDATE addressBook1 SET type = "friends" WHERE first_name IN ("Ishika", "Anju");
-UPDATE addressBook1 SET type = "family" WHERE first_name IN ("Kriti");
+update address_book_1 set name = "book1";
+update address_book_1 set type = "friends" where first_name in ("Anju", "Ishika");
+update address_book_1 set type = "family" where first_name in ("Kriti", "Sandhya");
+select * from address_book_1;
 
+-- output to above query
+-- Kriti	Jaiswal	Samastipur	Patna	Bihar	74185	919936574857	kritijaiswal7485@gmail.com	family	book1
+-- Sandhya	Sharma	Campel Road	Amritsar	Punjab	456002	91 9685741452	sandhyasharma47@gmail.com	family	book1
+-- Anju	Sharma	Danapur	Patna	Bihar	784575	91 9876542581	anju0174@gmail.com	friends	book1
+-- Ishika	Das	Adampur	Patna	Bihar	784572	91 9871473698	ishika7875@gmail.com	friends	book1
 
-SELECT * FROM addressBook1;
-+------------+-----------+---------+-------+-------+--------+---------------+----------------------------+---------+-------+
-| first_name | last_name | address | city  | state | zip    | phone         | email                      | type    | name  |
-+------------+-----------+---------+-------+-------+--------+---------------+----------------------------+---------+-------+
-| Kriti      | Jaiswal   | Kochi   | Patna | Bihar | 74185  | 919936574857  | kritijaiswal7485@gmail.com | family  | book1 |
-| Anju       | Sharma    | Danapur | Patna | Bihar | 784575 | 91 9876542581 | anju0174@gmail.com         | friends | book1 |
-| Ishika     | Das       | Adampur | Patna | Bihar | 784572 | 91 9871473698 | ishika7875@gmail.com       | friends | book1 |
-+------------+-----------+---------+-------+-------+--------+---------------+----------------------------+---------+-------+
-3 rows in set (0.00 sec)
 
 -- UC10
 -- get number of contacts by type
-SELECT type, count(type) FROM addressBook1 GROUP BY type;
-+---------+-------------+
-| type    | count(type) |
-+---------+-------------+
-| family  |           1 |
-| friends |           2 |
-+---------+-------------+
-2 rows in set (0.00 sec)
+select type, count(type) from address_book_1 group by type;
 
--- UC11
--- add contact to both friends and family
-INSERT INTO addressBook1 (first_name, last_name, address, city, state, zip, phone, email, type, name) VALUES
-	("Sakshi", "Singh", "Danapur", "Patna", "Bihar", "784575", "91 9872583694", "sakshisingh017@gmail.com", "friends", "book1"),
-    ("Palak", "Jain", "Indira Nagar", "Lucknow", "UP", "508755", "91 6392099360", "palakjain2574@gmail.com", "family", "book1");
+-- output to above query
+-- family	2
+-- friends	2
 
+-- UC12
+-- modify tables to create separate entities
+-- creating table for a new address book
+create table address_book_2(
+	person_id int not null,
+    address_id int not null,
+    name varchar(50),
+    type varchar(20),
+    primary key(person_id),
+    foreign key(person_id) references person_details(person_id),
+    foreign key(address_id) references address_details(address_id)
+);
 
-SELECT * FROM addressBook1;
-+------------+-----------+--------------+---------+-------+--------+---------------+----------------------------+---------+-------+
-| first_name | last_name | address      | city    | state | zip    | phone         | email                      | type    | name  |
-+------------+-----------+--------------+---------+-------+--------+---------------+----------------------------+---------+-------+
-| Kriti      | Jaiswal   | Kochi        | Patna   | Bihar | 74185  | 919936574857  | kritijaiswal7485@gmail.com | family  | book1 |
-| Anju       | Sharma    | Danapur      | Patna   | Bihar | 784575 | 91 9876542581 | anju0174@gmail.com         | friends | book1 |
-| Ishika     | Das       | Adampur      | Patna   | Bihar | 784572 | 91 9871473698 | ishika7875@gmail.com       | friends | book1 |
-| Sakshi     | Singh     | Danapur      | Patna   | Bihar | 784575 | 91 9872583694 | sakshisingh017@gmail.com   | friends | book1 |
-| Palak      | Jain      | Indira Nagar | Lucknow | UP    | 508755 | 91 6392099360 | palakjain2574@gmail.com    | family  | book1 |
-+------------+-----------+--------------+---------+-------+--------+---------------+----------------------------+---------+-------+
-5 rows in set (0.00 sec)
+-- creaating table for person details
+create table person_details(
+	person_id int not null auto_increment,
+    first_name varchar(50) not null,
+    last_name varchar(50),
+    phone varchar(20),
+    email varchar(320) not null,
+    primary key(person_id, first_name, email)
+);
+
+-- creating table for address details
+create table address_details(
+	address_id int not null auto_increment,
+    address varchar(100) not null,
+    city varchar(20) not null,
+    state varchar(20),
+    zip varchar(10),
+    primary key(address_id)
+);
+
+-- inserting some values into the tables
+insert into person_details(first_name, last_name, phone, email) values
+	("Kriti", "Jaiswal", "91 9875874859", "kritijaiswal2901@gmail.com"),
+    ("Rahul", "Sharma", "91 7487558456", "rahul@gmail.com"),
+	("Sandhya", "Sharma", "91 8757845875", "sandhya1485@gmail.com"),
+    ("Aditya", "Das", "91 9857425741", "aditya@gmail.com");
+insert into address_details(address, city, state, zip) values
+	("Hazipur", "Patna", "Bihar", "74185"),
+    ("Campel Road", "Amritsar", "Punjab", "456002"),
+    ("Indira Nagae", "Lucknow", "UP", "265748"),
+    ("Hitech City", "Hyderabad", "Telengana", "985745");
+insert into address_book_2(person_id, address_id, name, type) values
+	(1,1,"book2","Family"),
+    (2,2,"book2","Family"),
+    (3,3,"book2","Friend"),
+    (4,4,"book2","Friend");
+    
+-- UC13
+-- ability to execute all queries as in UC6,7,8,10
+
+-- retrieving people belonging to a city
+select * from person_details where person_id in 
+	(select person_id from address_book_2 as ab inner join address_details as ad on ab.address_id=ad.address_id where city = "Patna");
+
+-- output to above query is
+-- 1	Kriti	Jaiswal	91 9875874859	kritijaiswal2901@gmail.com
+
+-- count contacts by city or state
+select state, count(person_id) from address_book_2 as ab inner join address_details as ad on ab.address_id=ad.address_id where state = "Bihar";
+
+-- output to above query is
+-- Bihar	1
+
+-- get contacts sorted by name for given city
+select * from person_details where person_id in 
+	(select person_id from address_book_2 as ab inner join address_details as ad on ab.address_id=ad.address_id where city = "Patna")
+order by first_name asc;
+
+-- output to above query
+-- 1	Kriti	Jaiswal	91 9875874859	kritijaiswal2901@gmail.com
+
+-- count contacts by type
+select type, count(type) from address_book_2 group by type;
+
+-- output to above query
+-- Family	2
+-- Friend	2
+
+-- UC18
+-- adding the date_added attribute to contacts
+alter table address_book_2 add column date_added date null;
+update address_book_2 set date_added = "2023-12-11";
+select * from address_book_2;
+
+-- output to above query
+-- 1	1	book2	Family	2023-12-11
+-- 2	2	book2	Family	2023-12-11
+-- 3	3	book2	Friend	2023-12-11
+-- 4	4	book2	Friend	2023-12-11
